a6a586697f3c80c63c8371b7b1ef2572225a5801
gurukiran-github <125437618+gurukiran-github@users.noreply.github.com>
2023-02-17 21:51:53 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,6 +7,8 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "Name: Gurukiran";
   cout << "SRN: PES1UG20CS156";
+  cout << "After changing code";
+  cout << "Changed Successfully";
   cout << "I have successfully built and run\n";
   return 0;
 }
